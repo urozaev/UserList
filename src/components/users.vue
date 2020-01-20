ad33135ab2874d98ebe7fcbd52779d911e5da297
urozaev <maxim.urozaev@gmail.com>
2020-01-20 18:53:06 +0300

    connected inputs and users

--- src/components/users.vue
+++ src/components/users.vue
@@ -4,11 +4,11 @@
             <thead>
                 <tr>
                 <th scope="col">ID</th>
-                <th scope="col">Имя</th>
-                <th scope="col">Специальность</th>
-                <th scope="col">Телефон</th>
-                <th scope="col">День рождения</th>
-                <th scope="col">В архиве</th>
+                <th scope="col">Name</th>
+                <th scope="col">Birthday</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Role</th>
+                <th scope="col">Archive</th>
                 </tr>
             </thead>
             <tbody>
@@ -20,19 +20,19 @@
                     <td>{{user.role}}</td>
                     <td>{{user.isArchive}}</td>
                     <td>
-                        <button class="btn btn-info" type="button" v-b-modal.modalForm> Edit </button> / <button class="btn btn-danger" type="button"> Remove </button>
+                        <button class="btn btn-info" type="button" v-b-modal.modalForm @click="getUser(user)"> Edit </button> / <button class="btn btn-danger" type="button"> Remove </button>
                     </td>
                 </tr>
             </tbody>
         </table>
 
-    <div class="mt-3">
+    <div class="submit-list mt-3">
       Submitted Names:
       <div v-if="submittedNames.length === 0">--</div>
       <ul v-else class="mb-0 pl-3">
         <li v-for="(name, index) in submittedNames" :key="index">
-            <p>{{name}}</p>
-            <p>{{phone}}</p>
+            <p>{{user.name}}</p>
+            <p>{{user.phone}}</p>
         </li>
       </ul>
     </div>
@@ -84,17 +84,26 @@
         id="modalForm"
         centered
         ref="modal"
-        title="BootstrapVue"
+        title="User info"
         
         
         @ok="handleOk"
         >
         <form ref="form" @submit.stop.prevent="handleSubmit">
             <b-form-group :state="nameState" label="Name" label-for="name-input" invalid-feedback="Name is required">
-                <b-form-input id="name-input" v-model="users.name" :state="nameState" required></b-form-input>
+                <b-form-input id="name-input" v-model="user.name" :state="nameState" required></b-form-input>
+            </b-form-group>
+            <b-form-group :state="birthdayState" label="Birthday" label-for="birthday-input" invalid-feedback="birthday is required">
+                <b-form-input id="birthday-input" v-model="user.birthday" :state="birthdayState" v-mask="`##/##/####`" required></b-form-input>
             </b-form-group>
             <b-form-group :state="phoneState" label="Phone" label-for="phone-input" invalid-feedback="Phone is required">
-                <b-form-input id="phone-input" v-model="phone" :state="phoneState" v-mask="`+# (###) ### ## ##`" required></b-form-input>
+                <b-form-input id="phone-input" v-model="user.phone" :state="phoneState" v-mask="`+# (###) ### ## ##`" required></b-form-input>
+            </b-form-group>
+            <b-form-group :state="roleState" label="Role" label-for="role-input" invalid-feedback="Role is required">
+                <b-form-select id="role-input" v-model="user.role"  :state="roleState" :options="userRoles"></b-form-select>
+            </b-form-group>
+            <b-form-group :state="archiveState" label="Archive" label-for="archive-checkbox">
+                <b-form-checkbox id="archive-checkbox" v-model="user.isArchive" name="archive-checkbox"></b-form-checkbox>
             </b-form-group>
         </form>
     </b-modal>
@@ -113,8 +122,6 @@ export default {
     // },
     data() {
         return{
-            // userId: null,
-            // showModal: false,
             users: [  
                 {
                     "id": 1,
@@ -141,14 +148,24 @@ export default {
                     "birthday": "29.11.1990"
                 }
             ],
-            name: '',
-            isArchive: false,
-            role: "",
-            phone: "",
-            birthday: "",
+            user: {
+                name: '',
+                isArchive: false,
+                role: "",
+                phone: "",
+                birthday: ""
+            },
             nameState: null,
             phoneState: null,
-            submittedNames: []
+            birthdayState: null,
+            roleState: null,
+            archiveState: null,
+            submittedNames: [],
+            userRoles: [
+                { value: 'designer', text: 'designer' },
+                { value: 'manager', text: 'manager' },
+                { value: 'developer', text: 'developer' }
+            ]
         }
     },
     methods: {
@@ -160,6 +177,9 @@ export default {
       resetModal() {
         this.name = ''
         this.nameState = null
+      },
+      getUser(index) {
+          this.user = index
       },
       handleOk(bvModalEvt) {
         // Prevent modal from closing
@@ -173,7 +193,7 @@ export default {
           return
         }
         // Push the name to submitted names
-        this.submittedNames.push(this.name)
+        this.submittedNames.push(this.user.name)
         // Hide the modal manually
         this.$nextTick(() => {
           this.$bvModal.hide('modalForm')
@@ -192,5 +212,8 @@ export default {
         background: 0
         color: white
         border: 0
+
+    .submit-list
+        color: #fff
 
 </style>
