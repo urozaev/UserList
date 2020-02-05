81af64714db02ccd67a618164cc1d7b6dcab9f6b
urozaev <maxim.urozaev@gmail.com>
2020-02-05 04:02:44 +0300

    fixed bugs

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,9 +1,10 @@
 export default {
-    actions: {
-        async fetchUsers(ctx) {
-            const res = await fetch("http://localhost:8041/");
-            const users = await res.json();
-            ctx.commit('setUsers', users)
+    state: {
+        users: []
+    },
+    getters: {
+        allUsers(state) {
+            return state.users
         }
     },
     mutations: {
@@ -11,8 +12,8 @@ export default {
             state.users = users
         },
 
-        updateUser(state, userNew) {
-            fetch("http://localhost:8041/" + userNew.id, {
+        async updateUser(state, userNew) {
+            await fetch("http://localhost:8041/" + userNew.id, {
                 method: "PUT",
                 headers: {
                     'Accept': 'application/json',
@@ -30,8 +31,8 @@ export default {
             userOld.isArchive = userNew.isArchive
         },
 
-        createUser(state, newUser) {
-            fetch("http://localhost:8041/", {
+        async createUser(state, newUser) {
+            await fetch("http://localhost:8041/", {
                 method: "POST",
                 headers: {
                     'Accept': 'application/json',
@@ -42,8 +43,8 @@ export default {
             state.users.push(newUser)
         },
 
-        deleteUser(state, userId) {
-            fetch("http://localhost:8041/" + userId.id, {
+        async deleteUser(state, userId) {
+            await fetch("http://localhost:8041/" + userId.id, {
                 method: "DELETE",
                 headers: {
                     "Content-Type": "application/json"
@@ -52,12 +53,11 @@ export default {
             state.users.splice(state.users.indexOf(userId), 1)
         }
     },
-    state: {
-        users: []
-    },
-    getters: {
-        allUsers(state) {
-            return state.users
+    actions: {
+        async fetchUsers(ctx) {
+            const res = await fetch("http://localhost:8041/");
+            const users = await res.json();
+            ctx.commit('setUsers', users)
         }
-    },
+    }
 }
